0e13cc53d142132e0850a0a0de0e80ee95ee0d29
Kazunori SAKAMOTO <exkazuu@gmail.com>
2011-10-10 15:16:28 +0900

    Implemented unit move #10

---
+++ SquadKnights.Core/UnitAction.fs
@@ -0,0 +1,10 @@
+﻿module UnitAction
+
+open SquadKnights.Entity
+
+let unitMovement (war : War) (src : ChipPoint) (dst : ChipPoint) =
+    let unit = war.Units |> Map.find src
+    let movedUnits = war.Units |> Map.remove src |> Map.add dst unit  
+    { war with Units = movedUnits }
+
+    
