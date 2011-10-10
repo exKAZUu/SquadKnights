50e30c7f9e8c32e440e379372e6d3705badd1cb6
Kazunori SAKAMOTO <exkazuu@gmail.com>
2011-10-10 17:53:12 +0900

    Change the signature of moveUnit function.

--- SquadKnights.Core/UnitAction.fs
+++ SquadKnights.Core/UnitAction.fs
@@ -2,7 +2,7 @@
 
 open SquadKnights.Entity
 
-let moveUnit (war : War) (src : ChipPoint) (dst : ChipPoint) =
+let moveUnit (src : ChipPoint) (dst : ChipPoint) (war : War) =
     let unit = war.Units |> Map.find src
     let movedUnits = war.Units |> Map.remove src |> Map.add dst unit  
     { war with Units = movedUnits }
